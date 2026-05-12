330adadbfc82cb9a2f09ed3098cb65d701c3c778
ayiekoderrick-8068 <ayiekoderrick@gmail.com>
2026-05-12 13:13:23 +0300

    Implement all deliverables: fetch plants, add plant, toggle stock, search

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then(setPlants);
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants((prev) => [...prev, newPlant]);
+  }
+
+  const filteredPlants = plants.filter((p) =>
+    p.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search onSearch={setSearch} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
